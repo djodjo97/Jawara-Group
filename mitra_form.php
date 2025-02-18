5043ceff62dd542a1f1eb10834ac31caf42d9e77
Ciluukbaa <akmalfaruq03@gmail.com>
2025-02-18 17:17:55 +0700

    fix bugs, fix vendor asset, build option list in mitra menu

--- mitra_form.php
+++ mitra_form.php
@@ -13,118 +13,129 @@ $data = getData($id);
     </div>
     <div class="card-body">
       <form action="functions/function_mitra.php" method="POST" enctype="multipart/form-data" id="formAction">
+        <div class="form-group row">
+          <label for="genid" class="col-sm-2 col-form-label">Generasi</label>
+          <div class="col-sm-10">
+            <div class="input-group option-list">
+              <div class="input-group-prepend btn-option">
+                <button class="btn btn-primary" type="button" id="dataCode_gen"><i class="fas fa-caret-down"></i></button>
+              </div>
+              <input id="genId" name="gen" type="text" class="form-control" value="<?= $data['gen_id'] ?? ''; ?>" readonly hidden>
+              <input id="genDesc" type="text" class="form-control small" placeholder="Generasi" value="<?= $data['gen_name'] ?? ''; ?>" aria-describedby="button-addon" readonly required>
+            </div>
+          </div>
+        </div>
         <div class="row">
           <div class="form-group col-sm-4">
-            <label for="id" class="form-label">ID</label>
-            <input name="idmitra" type="text" class="form-control" value="<?= $data['id_mitra'] ?? ''; ?>" required>
+            <label for="mitraId" class="form-label">ID</label>
+            <input id="mitraId" name="idmitra" type="text" class="form-control" placeholder="ID Mitra" value="<?= $data['id_mitra'] ?? ''; ?>" required>
           </div>
-          <div class="form-group col-sm-4">
+          <div id="rowLeader" class="form-group col-sm-4">
             <label for="leader" class="form-label">Leader</label>
-            <input name="leader" type="text" class="form-control" value="<?= $data['leader_id'] ?? ''; ?>" required>
+            <div class="input-group option-list">
+              <div class="input-group-prepend btn-option">
+                <button class="btn btn-primary" type="button" id="dataCode_leader"><i class="fas fa-caret-down"></i></button>
+              </div>
+              <input id="leaderId" name="leader" type="text" class="form-control" value="<?= $data['leader_id'] ?? ''; ?>" readonly hidden>
+              <input id="leaderName" type="text" class="form-control small" placeholder="Leader" value="<?= $data['leader_name'] ?? ''; ?>" aria-describedby="button-addon" readonly required>
+            </div>
           </div>
           <div class="form-group col-sm-4">
             <label for="regnum" class="form-label">No. Registrasi</label>
-            <input name="regnum" type="text" class="form-control" value="<?= $data['registration_number'] ?? ''; ?>" required readonly>
+            <input id="regnum" name="regnum" type="text" class="form-control" placeholder="No. Registrasi" value="<?= $data['registration_number'] ?? ''; ?>" required readonly>
           </div>
         </div>
         <div class="form-group row">
           <label for="name" class="col-sm-2 col-form-label">Nama</label>
           <div class="col-sm-10">
-            <input name="name" type="text" class="form-control" value="<?= $data['name'] ?? ''; ?>" required>
+            <input name="name" type="text" class="form-control" placeholder="Nama" value="<?= $data['name'] ?? ''; ?>" required>
           </div>
         </div>
         <div class="form-group row">
           <label for="email" class="col-sm-2 col-form-label">Email</label>
           <div class="col-sm-10">
-            <input name="email" type="email" class="form-control" value="<?= $data['email'] ?? ''; ?>" required>
+            <input name="email" type="email" class="form-control" placeholder="Email" value="<?= $data['email'] ?? ''; ?>" required>
           </div>
         </div>
         <div class="form-group row">
           <label for="phone" class="col-sm-2 col-form-label">No. WhatsApp</label>
           <div class="col-sm-10">
-            <input name="phone" type="number" class="form-control" value="<?= $data['phone'] ?? ''; ?>" required>
+            <input name="phone" type="number" class="form-control" placeholder="No. WhatsApp" value="<?= $data['phone'] ?? ''; ?>" required>
           </div>
         </div>
         <div class="form-group row">
-          <label for="address" class="col-sm-2 col-form-label">Address</label>
+          <label for="address" class="col-sm-2 col-form-label">Alamat</label>
           <div class="col-sm-10">
             <textarea name="address" class="form-control"><?= $data['address'] ?? ''; ?></textarea>
           </div>
         </div>
-        <div class="form-group row">
-          <label for="genid" class="col-sm-2 col-form-label">Generasi</label>
-          <div class="col-sm-10">
-            <div class="input-group">
-              <div class="input-group-prepend">
-                <button class="btn btn-primary" type="button"><i class="fas fa-caret-down"></i></button>
-              </div>
-              <input name="gen" type="text" class="form-control small" value="<?= $data['gen_id'] ?? ''; ?>" aria-describedby="button-addon" readonly>
-            </div>
-          </div>
-        </div>
 
         <div class="form-group row">
-          <label for="idType" class="col-sm-2 col-form-label">Jenis ID</label>
+          <label for="idType" class="col-sm-2 col-form-label">No. Identitas</label>
           <div id="idCard" class="input-group col-sm-10">
-            <select id="idType" class="form-control col-sm-1">
+            <select id="idType" name="id_type" class="form-control col-sm-2">
               <option selected value="ktp">KTP</option>
               <option value="sim">SIM</option>
             </select>
-            <input name="ktp" type="text" class="form-control" placeholder="No. KTP" value="<?= $data['ktp'] ?? ''; ?>">
-            <input name="sim" type="text" class="form-control" placeholder="No. SIM" value="<?= $data['sim'] ?? ''; ?>" hidden>
+            <input id="ktp" name="ktp" type="number" class="form-control" placeholder="No. KTP" value="<?= $data['ktp'] ?? ''; ?>">
+            <input name="sim" type="number" class="form-control" placeholder="No. SIM" value="<?= $data['sim'] ?? ''; ?>" hidden>
           </div>
         </div>
 
-        <!-- <div class="form-group row">
-          <label for="idType" class="col-sm-2 col-form-label">Jenis ID</label>
-          <div class="col-md-10 row">
-            <div class="form-group col-4">
-              <select id="idType" class="form-control">
-                <option selected value="ktp">KTP</option>
-                <option value="sim">SIM</option>
-              </select>
-            </div>
-            <div id="idCard" class="form-group col-8">
-              <input name="ktp" type="text" class="form-control" placeholder="No. KTP" value="<?= $data['ktp'] ?? ''; ?>">
-              <input name="sim" type="text" class="form-control" placeholder="No. SIM" value="<?= $data['sim'] ?? ''; ?>" hidden>
-            </div>
-          </div>
-        </div> -->
-
         <div class="row">
           <div class="col-sm-6">
-            <div class="mb-3">
+            <div class="form-group option-list">
               <label for="up_i" class="form-label">Upper I</label>
-              <input name="up_i" type="text" class="form-control" value="<?= $data['upper_i'] ?? ''; ?>">
+              <div class="input-group">
+                <div class="input-group-prepend btn-option">
+                  <button class="btn btn-primary dataCode_up" type="button" id="dataCode_up_i"><i class="fas fa-caret-down"></i></button>
+                </div>
+                <input id="up_i" name="up_i" type="text" class="form-control" value="<?= $data['upper_i'] ?? ''; ?>" readonly hidden>
+                <input id="up_i_name" type="text" class="form-control small" placeholder="Upper I" value="<?= $data['upper_i_name'] ?? ''; ?>" aria-describedby="button-addon" readonly required>
+              </div>
             </div>
-            <div class="mb-3">
+            <div class="form-group option-list">
               <label for="up_ii" class="form-label">Upper II</label>
-              <input name="up_ii" type="text" class="form-control" value="<?= $data['upper_ii'] ?? ''; ?>">
+              <div class="input-group">
+                <div class="input-group-prepend btn-option">
+                  <button class="btn btn-primary dataCode_up" type="button" id="dataCode_up_ii"><i class="fas fa-caret-down"></i></button>
+                </div>
+                <input id="up_ii" name="up_ii" type="text" class="form-control" value="<?= $data['upper_ii'] ?? ''; ?>" readonly hidden>
+                <input id="up_ii_name" type="text" class="form-control small" placeholder="Upper II" value="<?= $data['upper_ii_name'] ?? ''; ?>" aria-describedby="button-addon" readonly required>
+              </div>
             </div>
-            <div class="mb-3">
+
+            <div class="form-group option-list">
               <label for="up_iii" class="form-label">Upper III</label>
-              <input name="up_iii" type="text" class="form-control" value="<?= $data['upper_iii'] ?? ''; ?>">
+              <div class="input-group">
+                <div class="input-group-prepend btn-option">
+                  <button class="btn btn-primary dataCode_up" type="button" id="dataCode_up_iii"><i class="fas fa-caret-down"></i></button>
+                </div>
+                <input id="up_iii" name="up_iii" type="text" class="form-control" value="<?= $data['upper_iii'] ?? ''; ?>" readonly hidden>
+                <input id="up_iii_name" type="text" class="form-control small" placeholder="Upper III" value="<?= $data['upper_iii_name'] ?? ''; ?>" aria-describedby="button-addon" readonly required>
+              </div>
             </div>
+
           </div>
           <div class="col-sm-6">
             <div class="mb-3">
               <label for="bonus_i" class="form-label">Bonus I</label>
-              <input name="bonus_i" type="text" class="form-control" value="<?= $data['bonus_i'] ?? ''; ?>">
+              <input name="bonus_i" type="text" class="form-control" placeholder="Bonus I" value="<?= $data['bonus_i'] ?? ''; ?>">
             </div>
             <div class="mb-3">
               <label for="bonus_ii" class="form-label">Bonus II</label>
-              <input name="bonus_ii" type="text" class="form-control" value="<?= $data['bonus_ii'] ?? ''; ?>">
+              <input name="bonus_ii" type="text" class="form-control" placeholder="Bonus II" value="<?= $data['bonus_ii'] ?? ''; ?>">
             </div>
             <div class="mb-3">
               <label for="bonus_iii" class="form-label">Bonus III</label>
-              <input name="bonus_iii" type="text" class="form-control" value="<?= $data['bonus_iii'] ?? ''; ?>">
+              <input name="bonus_iii" type="text" class="form-control" placeholder="Bonus III" value="<?= $data['bonus_iii'] ?? ''; ?>">
             </div>
           </div>
         </div>
-        <div>
+        <div class="mt-3 d-flex justify-content-end gap-1">
           <input type="hidden" id="inputAction" name="add">
           <button id="btnSave" class="btn btn-primary">Simpan</button>
-          <a class="btn btn-secondary" href="mitra.php">Batal</a>
+          <a class="btn btn-secondary ml-2" href="mitra.php">Batal</a>
         </div>
       </form>
     </div>
@@ -132,106 +143,43 @@ $data = getData($id);
 </div>
 <!-- /.container-fluid -->
 
-<script>
-  document.addEventListener("DOMContentLoaded", function() {
-    if (window.jQuery) {
-      $('.form-control[name="idmitra"]').data('col', 'id_mitra');
-      $('.form-control[name="regnum"]').data('col', 'registration_number');
-      $('.form-control[name="name"]').data('col', 'name');
-      $('.form-control[name="email"]').data('col', 'email');
-      $('.form-control[name="phone"]').data('col', 'phone');
-      $('.form-control[name="address"]').data('col', 'address');
-      $('.form-control[name="gen"]').data('col', 'gen_id');
-      $('.form-control[name="ktp"]').data('col', 'ktp');
-      $('.form-control[name="sim"]').data('col', 'sim');
-      $('.form-control[name="leader"]').data('col', 'leader_id');
-      $('.form-control[name="up_i"]').data('col', 'upper_i');
-      $('.form-control[name="up_ii"]').data('col', 'upper_ii');
-      $('.form-control[name="up_iii"]').data('col', 'upper_iii');
-      $('.form-control[name="bonus_i"]').data('col', 'bonus_i');
-      $('.form-control[name="bonus_ii"]').data('col', 'bonus_ii');
-      $('.form-control[name="bonus_iii"]').data('col', 'bonus_iii');
 
-      const params = new URLSearchParams(window.location.search);
-      if (params.get('id')) {
-        $('#title').text('Data Mitra');
-        $('#formAction').data('action', 'edit');
-        $('#inputAction').prop('name', 'edit');
-        $('#btnSave').text('Ubah');
-        $('.form-control[name="idmitra"').prop('readonly', true);
-      } else {
-        $('#title').text('Pendaftaran Mitra');
-        $('#formAction').data('action', 'add');
-        $('#inputAction').prop('name', 'add');
-        $('#btnSave').text('Simpan');
-        $('.form-control[name="idmitra"').prop('readonly', false);
-      }
+<!-- Modal Generation Data-->
+<div class="modal fade" id="fieldModal" tabindex="-1" aria-labelledby="fieldModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="fieldModalLabel">Generations</h5>
+        <button type="button" class="close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <table class="table table-sm table-hover" id="fieldData">
+          <thead>
+            <tr>
 
-      $('#idType').on('change', function() {
-        const idType = $(this).val();
-        if (idType == "ktp") {
-          $('#idCard input[name="ktp"]').prop('hidden', false);
-          $('#idCard input[name="sim"]').prop('hidden', true);
-        } else if (idType == "sim") {
-          $('#idCard input[name="ktp"]').prop('hidden', true);
-          $('#idCard input[name="sim"]').prop('hidden', false);
-        }
-      });
+            </tr>
+          </thead>
+          <tbody>
 
-      $('#formAction').on('change', '.form-control', function() {
-        $(this).addClass('row-change');
-      });
+          </tbody>
+        </table>
+        <div class="d-flex justify-content-center">
+          <div id="modalSpinner" class="spinner-border m-3" role="status">
+            <span class="sr-only">Loading...</span>
+          </div>
+        </div>
+      </div>
+      <div class="modal-footer">
 
-      $('#btnSave').on('click', function(e) {
-        e.preventDefault(); // Mencegah aksi default tombol
-        if (!$('#formAction').get(0).checkValidity()) {
-          $('#formAction').get(0).reportValidity()
-          return false;
-        }
+      </div>
+    </div>
+  </div>
+</div>
+<!-- Modal Generation Data End-->
 
-        if ($('#formAction').data('action') == "add") {
-          $('#formAction').submit();
-        } else {
-          const data = new Object();
-          let attr;
-          $('.row-change').each(function() {
-            attr = $(this).data('col');
-            data[attr] = $(this).val();
-          });
-          const dataId = $('.form-control[name="idmitra"]').val();
-          fetch('endpoint/api_mitra.php?id=' + dataId, {
-              method: 'PATCH',
-              headers: {
-                'Content-Type': 'application/json'
-              },
-              body: JSON.stringify(data)
-            })
-            .then(response => {
-              if (!response.ok) {
-                throw new Error(`HTTP error! Status: ${response.status}`);
-              }
-              return response.text();
-            })
-            .then(res => {
-              Swal.fire({
-                icon: "success",
-                title: "The data has been saved successfully!",
-                showConfirmButton: false,
-                timer: 1500
-              }).then(() => {
-                window.location.reload();
-              });
-            })
-            .catch(error => {
-              console.error("Terjadi kesalahan:", error);
-            });
-        }
-      });
-    } else {
-      console.error("jQuery belum tersedia!");
-    }
-  });
-</script>
+<script src="js/origin/mitra_form.js"></script>
 
 <?php
 include_once 'layout/footer.php';
