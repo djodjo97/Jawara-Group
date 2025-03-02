25161beb1e90c440aa0a0fe90699964aadbdd50a
Ciluukbaa <akmalfaruq03@gmail.com>
2025-03-02 14:40:51 +0700

    change layout profile menu

--- profile.php
+++ profile.php
@@ -16,7 +16,7 @@ $data  = getData();
           <!-- Profile picture help block-->
           <div class="font-italic text-muted mb-4">JPG or PNG no larger than 5 MB</div>
           <!-- Profile picture upload button-->
-          <button class="btn btn-primary" type="button">Upload new image</button>
+          <!-- <button class="btn btn-primary" type="button">Upload new image</button> -->
         </div>
       </div>
     </div>
