e27581bf149b2bfbdd4ee0525a1463ba906d3c50
djodjo97 <djody.aripin1@gmail.com>
2025-03-16 00:49:04 +0700

    Perubahan function_home.php,Penambahan struktur database tb_users dan tb_mitra current_time, penambahan grafik user dan mitra pada dashboard

--- functions/function_home.php
+++ functions/function_home.php
@@ -4,23 +4,37 @@ require_once 'koneksi.php';
 
 function getTotalMitra()
 {
-    global $conn;
-    $sql = "SELECT COUNT(*) AS total FROM tb_mitra";
-    $stmt = $conn->prepare($sql);
-    $stmt->execute();
-    $result = $stmt->get_result();
-    $data = $result->fetch_assoc();
-    $stmt->close();
-    return $data['total'];
+    {
+        global $conn;
+        $sql = "SELECT MONTH(created_at) AS bulan, COUNT(*) AS total FROM tb_mitra GROUP BY bulan";
+        $stmt = $conn->prepare($sql);
+        $stmt->execute();
+        $result = $stmt->get_result();
+    
+        $data = array_fill(0, 12, 0); // Default 12 bulan (Jan - Des)
+        while ($row = $result->fetch_assoc()) {
+            $data[$row['bulan'] - 1] = $row['total'];
+        }
+    
+        $stmt->close();
+        return $data;
+    }
 }
 function getTotalUsers()
 {
-    global $conn;
-    $sql = "SELECT COUNT(*) AS total FROM tb_user";
-    $stmt = $conn->prepare($sql);
-    $stmt->execute();
-    $result = $stmt->get_result();
-    $data = $result->fetch_assoc();
-    $stmt->close();
-    return $data['total'];
+    {
+        global $conn;
+        $sql = "SELECT MONTH(created_at) AS bulan, COUNT(*) AS total FROM tb_user GROUP BY bulan";
+        $stmt = $conn->prepare($sql);
+        $stmt->execute();
+        $result = $stmt->get_result();
+    
+        $data = array_fill(0, 12, 0); // Default 12 bulan (Jan - Des)
+        while ($row = $result->fetch_assoc()) {
+            $data[$row['bulan'] - 1] = $row['total'];
+        }
+    
+        $stmt->close();
+        return $data;
+    }
 }
